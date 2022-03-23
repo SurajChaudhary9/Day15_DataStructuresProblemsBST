92110b305ccc479f130c9c65c5868769643d58a3
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-03-23 14:26:19 +0530

    Added : Size Of BST

--- src/com/oops/ds/BST.java
+++ src/com/oops/ds/BST.java
@@ -1,10 +1,84 @@
 /**
- *@PROBLEM_STATEMENT : UC
+ *@PROBLEM_STATEMENT : UC2 ability to create the binary tree shown in the figure
+ * - Check if all are added with using size method in Binary Tree
  *@author Suraj Chaudhary
  *@Date 23-Mar-2022
  */
 package com.oops.ds;
 
-public class BST {
+class Node {
+	int data;
+	Node left;
+	Node right;
 
+	public Node(int data) {
+		this.data = data;
+		Node left = null;
+		Node right = null;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [data=" + data + ", left=" + left + ", right=" + right + "]";
+	}
+
+}
+
+public class BST {
+	int size = 0;
+
+	/*
+	 * @Purpose : Insert Elements in BST
+	 * 
+	 * @Return : Node
+	 */
+	public Node insert(Node node, int data) {
+
+		if (node == null) {
+			node = createNewNode(data);
+		}
+
+		if (data < node.data) {
+			node.left = insert(node.left, data);
+		} else if (data > node.data) {
+			node.right = insert(node.right, data);
+		}
+
+		return node;
+	}
+
+	/*
+	 * @Purpose : Create New Node For Elements in BST
+	 * 
+	 * @Return : MyNode
+	 */
+	private Node createNewNode(int data) {
+		Node mynode = new Node(data);
+		size += 1;
+		return mynode;
+	}
+
+	/*
+	 * @Purpose : Print InOrder Traversal in BST
+	 * 
+	 * @Return : Null
+	 */
+	public void printInorder(Node node) {
+		if (node == null) {
+			return;
+		}
+		printInorder(node.left);
+		System.out.print(node.data + "->");
+		printInorder(node.right);
+
+	}
+
+	/*
+	 * @Purpose : Getting Size Of BST
+	 * 
+	 * @Return : Size
+	 */
+	public int size() {
+		return size;
+	}
 }
